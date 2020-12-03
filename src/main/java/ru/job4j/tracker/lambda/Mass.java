0b80feac422fc1a7ff697447f8dcbcbc2ced5423
Айдер <emage.haf@gmail.com>
2020-12-03 19:06:49 +0200

    2.5. Ленивая загрузка [#249211]

---
+++ src/main/java/ru/job4j/tracker/lambda/Mass.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Mass {
+    public static void main(String[] args) {
+        String[] names = {
+                "Ivan",
+                "Petr"
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
+    }
+}
