20a6faa59c9d35c2bf36060be442f60199e77359
Айдер <emage.haf@gmail.com>
2020-12-04 09:51:52 +0200

    2.5. Ленивая загрузка [#249211]

--- src/main/java/ru/job4j/tracker/department/Departments.java
+++ src/main/java/ru/job4j/tracker/department/Departments.java
@@ -3,17 +3,6 @@ package ru.job4j.tracker.department;
 import java.util.*;
 
 public class Departments {
-
-    /**
-     * Чтобы реализовать метод fillGaps, нужно определить, какие элементы отсутствуют в системе.
-     * Для этого каждую входящую строку нужно разбить на одиночные элементы. Для этого можно использовать
-     * метод String.split("/"). Этот метод вернет массив одиночных элементов. Далее нужно через цикл последовательно
-     * складывать элементы и добавлять их в множество. Используйте в качестве промежуточного хранения LinkedHashSet.
-     *
-     * @param deps
-     * @return
-     */
-
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<>();
         for (String value : deps) {
@@ -22,7 +11,7 @@ public class Departments {
                 if (tmp.isEmpty()) {
                     tmp.add(el);
                     start = el;
-                } else if(deps.size() == value.split("/").length){
+                } else if (deps.size() == value.split("/").length) {
                     tmp.add(value);
                     break;
                 } else {
@@ -32,12 +21,5 @@ public class Departments {
             }
         }
         return new ArrayList<>(tmp);
-    }
-
-    public static void sortAsc(List<String> orgs) {
-
-    }
-
-    public static void sortDesc(List<String> orgs) {
     }
 }
--- src/main/java/ru/job4j/tracker/lambda/Reduce.java
+++ src/main/java/ru/job4j/tracker/lambda/Reduce.java
@@ -5,14 +5,14 @@ import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
 
 public class Reduce {
-
-   private static int loop(int to, BiFunction<Integer, Integer, Integer> func, Supplier<Integer> initValue) {
-       int rsl = initValue.get();
-       for (int index = 1; index <= to; index++) {
-           rsl = func.apply(rsl, index);
-       }
-       return rsl;
-   }
+    private static int loop(int to, BiFunction<Integer, Integer, Integer> func,
+                            Supplier<Integer> initValue) {
+        int rsl = initValue.get();
+        for (int index = 1; index <= to; index++) {
+            rsl = func.apply(rsl, index);
+        }
+        return rsl;
+    }
 
     public static int summation(int to) {
         BiFunction<Integer, Integer, Integer> func = new BinaryOperator<Integer>() {
